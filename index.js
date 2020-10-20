732f9a31671d2c164e742521955c96b248131d2f
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-20 10:20:39 +0000

    add list of students

---
+++ index.js
@@ -0,0 +1,27 @@
+//const students ={name: "Thando", mark: "30"};
+//const students = {name}
+
+
+//var args_string = JSON.stringify(obj);
+//console.log(args_string);
+//
+var list =[
+{name: "John", marks:30},
+ {name: "Kuhle", marks:59},
+{name: "Sizwe", marks:40},
+{name: "Atli", marks:36},
+{name: "Angel", marks:50},
+{name: "Nozie", marks:89},
+
+];
+console.log(student);
+
+//Display students
+
+//Calculate average
+
+
+
+//Get top achieve
+
+//Two top students
