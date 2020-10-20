154900b1a90a0e4e1e005902c77f66177fd4e9e0
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-20 11:27:04 +0000

    changed line 24

--- index.js
+++ index.js
@@ -5,21 +5,30 @@
 //var args_string = JSON.stringify(obj);
 //console.log(args_string);
 //
-var list =[
-{name: "John", marks:30},
- {name: "Kuhle", marks:59},
-{name: "Sizwe", marks:40},
-{name: "Atli", marks:36},
-{name: "Angel", marks:50},
-{name: "Nozie", marks:89},
+//var list =[
+//{name: "John", marks:30},
+ //{name: "Clie", marks:59},
+//{name: "Sizwe", marks:40},
+//{name: "Atli", marks:36},
+//{name: "Angel", marks:50},
+//{name: "Nozie", marks:89},
 
-];
-console.log(student);
+//];
+//console.log(student);
 
 //Display students
 
-//Calculate average
+//Calculate average of students
+var arr = [{ name: "John", Marks: "30" }, { name: "Clie", marks: "59" }, { name: "Sizwe", Marks: "40" }, { name: "Atli", score: "36" }, { name: "Nozie", score: "89" }],
+    avgScore = arr.reduce(function (r, person) {
+        if (person.name === "Atli") {
+            r.sum += +person.score;
+            r.avg = r.sum / ++r.count;
+        }
+        return r;
+    }, { sum: 0, count: 0, avg: 0 }).avg;
 
+console.log(avgScore);
 
 
 //Get top achieve
