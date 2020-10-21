588dd337fb04e9d4b261276f9dc74e686012b852
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-10-21 13:37:50 +0000

    added var mystring = JSON.stringfy(arr) in line number 22

--- index.js
+++ index.js
@@ -5,18 +5,23 @@
 //var args_string = JSON.stringify(obj);
 //console.log(args_string);
 //
-//var list =[
-//{name: "John", marks:30},
- //{name: "Clie", marks:59},
-//{name: "Sizwe", marks:40},
-//{name: "Atli", marks:36},
-//{name: "Angel", marks:50},
-//{name: "Nozie", marks:89},
+var list =[
+{name: "John", marks:30},
+ {name: "Kuhle", marks:59},
+{name: "Sizwe", marks:40},
+{name: "Atli", marks:36},
+{name: "Angel", marks:50},
+{name: "Nozie", marks:89},
 
-//];
-//console.log(student);
+];
+console.log(student);
 
-//Display students
+//Display students names
+var arr = [ "John", "Kuhle", "Sizwe", "Atli", "Angel", "Nozie"];
+
+var myString = JSON.stringify(arr);
+console.log(arr);
+
 
 //Calculate average of students
 var arr = [{ name: "John", Marks: "30" }, { name: "Clie", marks: "59" }, { name: "Sizwe", Marks: "40" }, { name: "Atli", score: "36" }, { name: "Nozie", score: "89" }],
@@ -32,5 +37,7 @@ console.log(avgScore);
 
 
 //Get top achieve
+//
+
 
 //Two top students
